6de85363e2b033c1dc43fe0bbf08e28f2c2b59ab
Nikitanovvval <nikitanovvval@gmail.com>
2022-07-03 23:56:12 +0500

    updated db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from config import Config
+from dao.model.user_model import User
 from setup_db import db
 from views.auth_view import auth_ns
 from views.movies_view import movie_ns
@@ -23,6 +24,19 @@ def register_extensions(app):
     api.add_namespace(genre_ns)
     api.add_namespace(user_ns)
     api.add_namespace(auth_ns)
+    create_data(app, db)
+
+
+def create_data(app, db):
+    with app.app_context():
+        db.create_all()
+
+        u1 = User(username="vasya", password="my_little_pony", role="user")
+        u2 = User(username="oleg", password="qwerty", role="user")
+        u3 = User(username="oleg", password="P@ssw0rd", role="admin")
+
+        with db.session.begin():
+            db.session.add_all([u1, u2, u3])
 
 
 config = Config()
